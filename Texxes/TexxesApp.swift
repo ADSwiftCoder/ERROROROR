74be31a5b17ea251a829008b0ec0caf223124f9a
Adrian Roger Menezes <adrianrogermenezes10@gmail.com>
2023-09-16 19:09:49 +0400

    Initial Commit

---
+++ Texxes/TexxesApp.swift
@@ -0,0 +1,17 @@
+//
+//  TexxesApp.swift
+//  Texxes
+//
+//  Created by Adrian Menezes on 16/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct TexxesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
